eb73e83c4a29d0de00f61239993844780e547d36
CedricSJSelph <koastiebratt@gmail.com>
2022-10-10 22:07:59 -0400

    Some additions to back end, tested functionality. Works well so far

--- src/Main.java
+++ src/Main.java
@@ -17,30 +17,45 @@ public class Main {
 
             Scanner scan = new Scanner(System.in);
             System.out.print("What ticker would like info on? (Enter \"EXIT\" to stop): ");
-            String Stock = scan.next();
-            String[] info = stockInformationAPI(Stock);
+            String Stock = scan.nextLine();
+            if((Stock.equals("EXIT")) || (Stock.equals("exit"))) {
+                Answer = "EXIT";
+            }else{
+                String[] info = stockInformationAPI(Stock);
 
-            StockInfo stock = new StockInfo(Float.parseFloat(info[0]),
-                                            Float.parseFloat(info[1]),
-                                            Float.parseFloat(info[2]),
-                                            Float.parseFloat(info[3]),
-                                            Float.parseFloat(info[4]),
-                                            Stock);
+                StockInfo stock = new StockInfo(
+                        Float.parseFloat(info[0]), //Open
+                        Float.parseFloat(info[1]), //High
+                        Float.parseFloat(info[2]), //Low
+                        Float.parseFloat(info[3]), //Current
+                        Float.parseFloat(info[4]), //Volume
+                        Stock);                    //Name
 
-            System.out.println(stock.getName());
-            System.out.println("~~~~~~~~~~~~~~~~~~~~");
-            System.out.println("Open: "  + stock.getOpen());
-            System.out.println("High: " + stock.getHigh());
-            System.out.println("Low: " + stock.getLow());
-            System.out.println("Current: " + stock.getCurrent());
-            System.out.println("Volume: " + stock.getVolume());
+                System.out.println();
+                System.out.println(stock.getName());
+                System.out.println("~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Open: " + stock.getOpen());
+                System.out.println("High: " + stock.getHigh());
+                System.out.println("Low: " + stock.getLow());
+                System.out.println("Current: " + stock.getCurrent());
+                System.out.println("Volume: " + stock.getVolume());
 
+            }
         }while(Answer!="EXIT");
+
+        System.out.println("You chose to exit, have a nice day <3");
     }
 
     public static String[] stockInformationAPI(String stockName){
         String StockInformation[] = new String[5];
             //Do API stuff
+        //Temp data:
+        StockInformation[0] = "0.0";
+        StockInformation[1] = "0.0";
+        StockInformation[2] = "0.0";
+        StockInformation[3] = "0.0";
+        StockInformation[4] = "0.0";
+
         return StockInformation;
     }
 }
